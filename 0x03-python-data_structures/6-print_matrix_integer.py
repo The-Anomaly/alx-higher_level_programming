513cfff7d0ec735292e9bd5273975f70e79c6d60
The-Anomaly <favouradekogbe@gmail.com>
2022-06-07 02:37:47 +0000

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        pass
+    else:
+        for i in matrix:
+            for j in range(len(i)):
+                print("{:d}".format(i[j]), end ="")
+                if j != (len(i) - 1):
+                    print(" ", end="")
+            print()
