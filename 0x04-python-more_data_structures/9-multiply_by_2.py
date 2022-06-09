f2de52a107f2e7f19bff45ace061b4d018d92cd3
The-Anomaly <favouradekogbe@gmail.com>
2022-06-09 22:31:44 +0000

    9-multiply_by_2.py update

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     if not a_dictionary:
         pass
-    n = {x : a_dictionary[x]*2 for x in a_dictionary}
+    n = {x: a_dictionary[x]*2 for x in a_dictionary}
     return n
