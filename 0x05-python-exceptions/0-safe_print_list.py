e2c56d839ab6e94ce62b35bec0267ac96df92864
The-Anomaly <favouradekogbe@gmail.com>
2022-06-20 21:18:12 +0000

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     c = 0
-    try:
-        for li in my_list:
+
+    for li in my_list:
+        try:
             if (c < x):
                 print("{}".format(li), end="")
                 c += 1
-        print()
 
-    except:
-        print("Failed")
+        except:
+            print("Failed")
+
+    print()
 
     return c
