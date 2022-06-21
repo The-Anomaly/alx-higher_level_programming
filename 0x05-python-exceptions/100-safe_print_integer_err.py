0cde8cf3a603155c656a2f69c91771a2a9969152
The-Anomaly <favouradekogbe@gmail.com>
2022-06-21 00:42:00 +0000

    update print integer err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
 
-    except ValueError as te:
+    except (TypeError, ValueError) as te:
         sys.stderr.write("Exception : {}\n".format(te))
         return False
-
