40dfc51fc586e126bfa3a7b47a8dc8b08939a45e
The-Anomaly <favouradekogbe@gmail.com>
2022-06-21 00:05:02 +0000

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    c = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            c += 1
+
+        except IndexError:
+            raise
+            break
+
+        except (TypeError, ValueError):
+            pass
+    print()
+    return c
