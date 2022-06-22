059ff7545ff19e480add4328cdd6390c8125aa95
The-Anomaly <favouradekogbe@gmail.com>
2022-06-22 01:26:39 +0000

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""A square class"""
+
+
+class Square:
+    """Derives a square"""
+    def __init__(self, size=0):
+        """Initialize attributes
+
+        Args:
+            self (int): value to initialize to `size`
+
+        Note:
+            Do not include the `self` paramtere in the ``Args`` section.
+
+        Raises:
+            TypeError: when `size` isn't an integer
+            ValueError: `size` is less than 0
+
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate area of the square
+
+        Returns:
+            The area of the square
+
+        """
+
+        return self.__size ** 2
