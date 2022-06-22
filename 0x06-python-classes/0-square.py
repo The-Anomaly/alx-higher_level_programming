d87ad9a3652b3b1ac659773c59d45d478bad7c50
The-Anomaly <favouradekogbe@gmail.com>
2022-06-22 01:14:08 +0000

    add documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Represent a square class"""
+
+
 class Square:
+    """An empty class that defines a square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-class Square:
+"""A square class"""
 
+
+class Square:
+    """Derives a square"""
     def __init__(self, size):
+        """Initialize attributes
+        Args:
+            self (int): value to initialize to `size`
+        Note:
+            Do not include the `self` paramtere in the ``Args`` section.
+        """
         self.__size = size
