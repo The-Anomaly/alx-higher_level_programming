2f6526d967a655575ae0a04b1f1e385eb753ba30
The-Anomaly <favouradekogbe@gmail.com>
2022-07-27 23:59:41 +0000

    insert number

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+
+/*
+* insert_node - insert node into a sorted linked list
+* @head: pointer to the linked list
+* @number: number to the inserted
+*
+* Description: insert a node into a sorted linked list
+*
+* Return: address of new node
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *current = *head;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	while (current->next != NULL)
+	{
+		if (current->n < number && current->next->n > number)
+		{
+			new_node->n = number;
+			new_node->next = current->next;
+			current->next = new_node;
+			return (new_node);
+		}
+		current = current->next;
+	}
+	return (&head);
+}
