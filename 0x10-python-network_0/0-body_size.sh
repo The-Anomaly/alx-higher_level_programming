f6009ba461e435fabd6a29bef3a56f17363487dd
The-Anomaly <favouradekogbe@gmail.com>
2022-09-22 17:18:21 -0700

    0-body_size.sh update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl -sI "$1" | grep Content-Length | cut -d " " -f2
+# script that takes a URl, sends a request
+# and displays the size of the body of the response
+curl -sI "$1" | grep Content-Length
