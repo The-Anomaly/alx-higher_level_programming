748bc8aec7ff2c4db5845d41a2cf1197a78aaf67
Favour Adekogbe <49334193+The-Anomaly@users.noreply.github.com>
2022-09-23 02:54:14 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me and display the server message
-curl -sL -d "user_id=98" -H "origin: HolbertonSchool" "0.0.0.0:5000/catch_me" -X PUT
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
