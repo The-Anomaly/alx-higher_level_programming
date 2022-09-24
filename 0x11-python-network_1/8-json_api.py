982da6a03b0b8691cf1bba377581c46cf299f2b9
The-Anomaly <favouradekogbe@gmail.com>
2022-09-24 04:32:15 +0000

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A script that
+- takes in a letter
+- sends a POST request to http://0.0.0.0:5000/search_user
+- with the letter as a parameter.
+"""
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    data = {'q': ""}
+    data['q'] = argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+
+    r = requests.post(url, data=data)
+    try:
+        res =  r.json()
+        if res == {}:
+            print("No result")
+        else:
+            print(f"[{r.get('id')}] {r.get('name')}")
+    except ValueError:
+        print("Not a valid JSON")
