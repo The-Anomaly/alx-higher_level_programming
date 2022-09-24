6bb381e6e37f9411a3ce948e276975658bc88fe6
Favour Adekogbe <49334193+The-Anomaly@users.noreply.github.com>
2022-09-24 05:09:54 +0100

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,5 +8,7 @@
 import requests
 from sys import argv
 
-r = requests.get(argv[1])
-print(r.headers.get('X-Request-Id'))
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    print(r.headers.get("X-Request-Id"))
