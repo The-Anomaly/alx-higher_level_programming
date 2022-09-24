6e731e3d5fdf6bc31939a45657cc6b8b3f402b8e
The-Anomaly <favouradekogbe@gmail.com>
2022-09-24 03:56:39 +0000

    5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A script that
+- takes a URL
+- sends a request to the URL
+- displays the value of the variable X-Request-ID in the response header
+"""
+
+import requests
+from sys import argv
+
+r = requests.get(argv[1])
+print(f"{r.headers.get('X-Request-ID')}")
